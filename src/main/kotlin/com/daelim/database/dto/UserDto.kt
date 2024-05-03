ef785eb0364e4980f376225a885b553148735508
YJ <gg3582@naver.com>
2024-05-03 16:40:54 +0900

    Redis를 위한 SQL DB TABLE 구현

--- src/main/kotlin/com/daelim/database/dto/UserDto.kt
+++ src/main/kotlin/com/daelim/database/dto/UserDto.kt
@@ -13,7 +13,9 @@ data class User(
     var username: String,
     var password: String // 실제 애플리케이션에서는 비밀번호를 해시하여 저장
 )
-/*
+/* 데이터베이스 설정
+-> API를 활성화 하기 위하여 DB테이블 user 생성 명령어 후 Redis 서버 start 하여 세션 관리
+
 CREATE TABLE `user` (
 	`id` BIGINT(19) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_0900_ai_ci',
