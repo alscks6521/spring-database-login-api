ad20611eada6eebb7a5f8c15e1e67b659d21c3a6
arachne1102 <s1284123@gmail.com>
2024-05-07 19:00:36 +0900

    Controller 기능 부분 구현

--- src/main/kotlin/com/daelim/database/controller/UserController.kt
+++ src/main/kotlin/com/daelim/database/controller/UserController.kt
@@ -1,46 +1,50 @@
 package com.daelim.database.controller
 
 import com.daelim.database.dto.User
+import com.daelim.database.dto.UserUpdateDTO
 import com.daelim.database.service.UserService
+import jakarta.persistence.Id
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class UserController(private val userService: UserService) {
-    @PostMapping("/register")
-    fun register(
-        @RequestParam username: String,
-        @RequestParam password: String
+
+    @PutMapping("/users/{userId}")
+    fun updateUser(
+        @PathVariable userId: Long,
+        @RequestBody updateDto: UserUpdateDTO
     ): ResponseEntity<User> {
-        return ResponseEntity.ok(userService.registerUser(username, password))
-    }
-
-
-    @GetMapping("/login")
-    fun login(
-        @RequestParam username: String,
-        @RequestParam password: String
-    ): ResponseEntity<String> {
-        return if (userService.validateUser(username, password)) {
-            ResponseEntity.ok(userService.createSession(username))
-        } else {
+        return try {
+            val updatedUser = userService.updateUser(userId, updateDto)
+            ResponseEntity.ok(updatedUser)
+        } catch (e: IllegalArgumentException) {
             ResponseEntity.notFound().build()
         }
     }
-    @GetMapping("/check")
-    fun check(
+
+    @DeleteMapping("/users/{userId}")
+    fun deleteUser(
+        @RequestParam userId: Long
+    ): ResponseEntity<Void> {
+        return try {
+            userService.deleteUser(userId)
+            ResponseEntity.noContent().build()
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/sessionUpdate")
+    fun sessionUpdate(
         @RequestParam username: String,
         @RequestParam sessionId: String
     ): ResponseEntity<String> {
-        return if (userService.checkSession(username, sessionId)) {
-            ResponseEntity.ok("Session valid")
+        return if (userService.updateSession(username, sessionId)) {
+            ResponseEntity.ok("Session Update Success!")
         } else {
             ResponseEntity.notFound().build()
         }
     }
-
 
 }
